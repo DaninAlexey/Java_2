3990eeb1e39c1e6ce690afae79ae6d66e534603a
DaninAlexey <dalex772@yandex.ru>
2021-12-26 00:33:27 +0700

    ДЗ-3 курс JAVA-2

---
+++ src/lesson3/Lesson3Main.java
@@ -0,0 +1,61 @@
+package lesson3;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.System.lineSeparator;
+
+public class Lesson3Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> array = new ArrayList<>();
+        array.add("Альфа");
+        array.add("Бета");
+        array.add("Гамма");
+        array.add("Дельта");
+        array.add("Эпсилон");
+        array.add("Альфа");
+        array.add("Альфа");
+        array.add("Бета");
+        array.add("Омега");
+        array.add("Альфа");
+        array.add("Эпсилон");
+
+        searchUniqueWords(array);
+        System.out.println("----------------------------");
+        searchNumberAllWord(array);
+
+        TelephoneDirectory telephoneDirectory = new TelephoneDirectory();
+        telephoneDirectory.add("123-45-67", "Иванов");
+        telephoneDirectory.add("123-45-68", "Петров");
+        telephoneDirectory.add("123-45-69", "Сидоров");
+        telephoneDirectory.add("123-45-70", "Иванов");
+        telephoneDirectory.add("123-45-71", "Иванов");
+        telephoneDirectory.add("123-45-72", "Петров");
+
+        Set<String> listOfSurname = new HashSet<>();
+        listOfSurname = telephoneDirectory.get("Петров");
+        System.out.println(listOfSurname);
+    }
+
+    private static void searchUniqueWords(ArrayList array) {
+        for (int i = 0; i < array.size(); i++) {
+            if (array.indexOf(array.get(i)) == array.lastIndexOf(array.get(i)))
+                System.out.println(array.get(i));
+            lineSeparator();
+        }
+    }
+
+    private static void searchNumberAllWord(ArrayList array) {
+        for (int i = 0; i < array.size(); i++) {
+            if (i == array.indexOf(array.get(i))) {
+                int number = 1;
+                for (int j = i + 1; j < array.size(); j++)
+                    if ((array.get(i)).equals(array.get(j)))
+                        number++;
+                System.out.printf("Строка <%s> встречается %d раз %n", array.get(i), number);
+            }
+        }
+    }
+}
---
+++ src/lesson3/TelephoneDirectory.java
@@ -0,0 +1,40 @@
+package lesson3;
+
+import java.util.*;
+
+public class TelephoneDirectory {
+    private Map<String, String> phoneDirectory;
+
+    public TelephoneDirectory() {
+        phoneDirectory = new HashMap<>();
+    }
+
+    public void add(String phone, String name) {
+        phoneDirectory.put(phone, name);
+    }
+
+    public Set<String> get(String name) {
+        Set<String> needPhoneNumber = new HashSet<>();
+        for (Map.Entry<String, String> o : phoneDirectory.entrySet())
+        {
+            if (name.equals(o.getValue())) {
+                needPhoneNumber.add(o.getKey());
+                System.out.println("-------------");
+            }
+        }
+        return (needPhoneNumber);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TelephoneDirectory)) return false;
+        TelephoneDirectory that = (TelephoneDirectory) o;
+        return Objects.equals(phoneDirectory, that.phoneDirectory);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(phoneDirectory);
+    }
+}
